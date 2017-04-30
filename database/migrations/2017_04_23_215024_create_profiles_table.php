99fc3b94a83e86208b8ce152dec96f59aa6fe7dd
Jordy Wijman <jordywijman@gmail.com>
2017-04-30 14:51:57 +0200

    Initial setup

---
+++ database/migrations/2017_04_23_215024_create_profiles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->dateTime('birth_date');
+            $table->string('notification_token');
+            $table->unique('notification_token');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
