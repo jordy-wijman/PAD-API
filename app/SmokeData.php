aea03dd27d78f3b4de395637426e34acc3c4ae51
Jordy Wijman <jordywijman@gmail.com>
2017-05-17 15:17:45 +0200

    Model fix

--- app/SmokeData.php
+++ app/SmokeData.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class SmokeData extends Model
 {
+    protected $table = 'smoke_data';
+
     public function profile()
     {
         return $this->belongsTo('App\Profile');
