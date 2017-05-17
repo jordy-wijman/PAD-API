deb73378f837e24d1c5ece780c57792000303fb9
Jordy Wijman <jordywijman@gmail.com>
2017-05-17 15:07:04 +0200

    Another route fix

--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,9 @@ Route::get('/home', 'HomeController@index');
 Route::get('/profile', 'ProfileController@index');
 Route::post('/notification/send', 'NotificationController@send');
 
-Route::post('/api/profile/register', 'Api/ProfileController@register');
+Route::post('/api/profile/register', 'Api\ProfileController@register');
 
-Route::post('/api/alarm/add', 'Api/AlarmController@add');
-Route::post('/api/alarm/remove', 'Api/AlarmController@remove');
+Route::post('/api/alarm/add', 'Api\AlarmController@add');
+Route::post('/api/alarm/remove', 'Api\AlarmController@remove');
 
-Route::post('/api/smoke_data/add', 'Api/SmokeDataController@add');
+Route::post('/api/smoke_data/add', 'Api\SmokeDataController@add');
