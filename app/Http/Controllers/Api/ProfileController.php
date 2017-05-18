5e2456664a0748d230f279130d5ba49558804907
Jordy Wijman <jordywijman@gmail.com>
2017-05-18 14:03:48 +0200

    Added some new columns for profile

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -15,6 +15,9 @@ class ProfileController extends Controller
             'last_name' => 'required|max:50',
             'birth_date' => 'required|date',
             'notification_token' => 'required|min:150|max:155',
+            'price_per_pack' => 'required|between:0,99.99',
+            'cigarettes_per_day' => 'required|integer|digits_between:0,50',
+            'cigarettes_per_pack' => 'required|integer|digits_between:0,50'
         ];
 
         $validation = Validator::make($request->all(), $rules);
@@ -36,6 +39,9 @@ class ProfileController extends Controller
         $profile->last_name = $request->last_name;
         $profile->birth_date = $request->birth_date;
         $profile->notification_token = $request->notification_token;
+        $profile->price_per_pack = $request->price_per_pack;
+        $profile->cigarettes_per_day = $request->cigarettes_per_day;
+        $profile->cigarettes_per_pack = $request->cigarettes_per_pack;
 
         $profile->save();
 
