dabe3dbc279f87cad502bd55274475f6e8664125
Jordy Wijman <jordywijman@gmail.com>
2017-06-05 10:37:05 +0200

    Another small fix with registering

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Profile;
+use App\SmokeData;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ProfileController extends ApiController
@@ -33,8 +35,13 @@ class ProfileController extends ApiController
         $profile->cigarettes_per_day = $request->cigarettes_per_day;
         $profile->cigarettes_per_pack = $request->cigarettes_per_pack;
         $profile->stop_date = $request->stop_date;
-
         $profile->save();
+
+        $smokeData = new SmokeData;
+        $smokeData->profile_id = $profile->id;
+        $smokeData->amount = 1;
+        $smokeData->time_smoked = Carbon::now();
+        $smokeData->save();
 
         return $this->getAllInformation($request);
     }
